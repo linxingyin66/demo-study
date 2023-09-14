33b22f4111ace0b25bde40a5a8c4cf54c9cc5c64
LinXingYin <linxingyin_sky_66@163.com>
2023-09-14 13:52:51 +0800

    spring cache...

---
+++ spring_cache_demo/src/main/java/com/heima/spring_cache/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.heima.spring_cache.controller;
+
+import com.heima.spring_cache.entity.User;
+import com.heima.spring_cache.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/test")
+    public String test(){
+        System.out.println("连接成功!");
+        return "OK";
+    }
+
+    /**
+     * CachePut：将方法返回值放入缓存
+     * value：缓存的名称，每个缓存名称下面可以有多个key
+     * key：缓存的key
+     */
+    @PostMapping
+    @CachePut(value = "userCache", key = "#user.id")//key的生成：userCache::1
+    public User save(@RequestBody User user){
+        userMapper.insert(user);
+        return user;
+    }
+
+    @DeleteMapping
+    @CacheEvict(cacheNames = "userCache",key = "#id")//删除某个key对应的缓存数据
+    public void deleteById(Long id){
+        userMapper.deleteById(id);
+    }
+
+	@DeleteMapping("/delAll")
+    @CacheEvict(cacheNames = "userCache",allEntries = true)//删除userCache下所有的缓存数据
+    public void deleteAll(){
+        userMapper.deleteAll();
+    }
+
+    /**
+     * Cacheable：在方法执行前spring先查看缓存中是否有数据，如果有数据，则直接返回缓存数据；若没有数据，调用方法并将方法返回值放到缓存中
+     * value：缓存的名称，每个缓存名称下面可以有多个key
+     * key：缓存的key
+     */
+    @GetMapping
+    @Cacheable(cacheNames = "userCache",key="#id")
+    public User getById(Long id){
+        User user = userMapper.getById(id);
+        return user;
+    }
+
+}
